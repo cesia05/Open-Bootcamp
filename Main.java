da3cf1981f0c9d1b13dfec7ced112a0b96d6caaf
cesia05 <114003909+cesia05@users.noreply.github.com>
2023-06-02 18:22:10 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,68 @@
+// Ejercicios tema 4
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numero = 22;
+        System.out.println("Ejecutando if");
+
+        if(numero > 0) {
+            System.out.println("El numero es positivo");
+        }else if (numero < 0) {
+            System.out.println("El numero es negativo");
+        }else {
+            System.out.println("El número es 0");
+        }
+
+
+        System.out.println("Ejecutando while");
+        int numeroWhile =-2;
+
+        while(numeroWhile < 3){
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+
+
+        System.out.println("Ejecutando do-while");
+        do{
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        } while (numeroWhile < 3);
+
+
+
+        System.out.println("Ejecutando for");
+        for( int numeroFor = 0; numeroFor <= 3; numeroFor++){
+            System.out.println(numeroFor);
+        }
+
+
+        System.out.println("Ejecutando switch");
+        String estacion = "verano";
+
+        switch (estacion){
+            case "primavera":
+                System.out.println("la estacion es primavera ");
+                break;
+            case "verano":
+                System.out.println("la estacion es verano ");
+                break;
+            case "otoño":
+                System.out.println("la estacion es otoño ");
+                break;
+            case "invierno":
+                System.out.println("la estacion es invierno ");
+                break;
+            default:
+                System.out.println("Ingresa una estacion valida");
+        }
+
+
+
+
+
+    }
+
+
+}
